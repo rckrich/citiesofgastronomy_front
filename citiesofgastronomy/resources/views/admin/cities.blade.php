7f18bfc7fca3f2664a085910816f96cbf6aa8527
rociomoreno1 <rmoreno@walook.com.mx>
2023-12-05 09:42:03 -0600

    validación de guardados y correcciones

--- citiesofgastronomy/resources/views/admin/cities.blade.php
+++ citiesofgastronomy/resources/views/admin/cities.blade.php
@@ -11,13 +11,15 @@
                 </div>
                 <div class="col-12 px-0 text-right row mx-0 py-2">
                     <div class="col-lg-4 col-md-6 col-sm-12 col-12 px-2 ms-0 ms-lg-auto ms-md-auto">
-                        <form action="/admin/citiesSearch" method="POST" id="formSerch"  >
+                        <form action="/admin/cities" method="POST" id="formSerch"  >
                             @csrf
                             <div class="input-group">
                                 <span class="input-group-text" id="basic-addon1">
                                     <img src="{{asset('assets/icons/search_dark.svg')}}" onclick="searchlist()"/>
                                 </span>
-                                <input id="search_box" name="search_box" class="form-control me-2" type="search" placeholder="{{__('cities.admin.search_ph')}}" aria-label="{{__('cities.admin.search_ph')}}" aria-describedby="basic-addon1">
+                                <input id="search_box" name="search_box" value="<?= $search_box?>" class="form-control me-2" type="search" placeholder="{{__('cities.admin.search_ph')}}" aria-label="{{__('cities.admin.search_ph')}}" aria-describedby="basic-addon1">
+                                <input type="hidden" id="page" name="page"
+                                                value="<?php if($search_box!=''){echo  $page;}else{echo '1';};?>">
                             </div>
                         </form>
                     </div>
@@ -29,6 +31,11 @@
                     </div>
                 </div>
             </div>
+            <?php if($st == '1'){?>
+            <div class="alert alert-success" role="alert" id="alertMessage">
+                "City was successfully created
+            </div>
+            <?php };?>
             <div class="row mx-0 pt-4">
                 <table class="table table-fixed">
                     <thead class="">
@@ -68,6 +75,16 @@
                     @endif
                     </tbody>
                 </table>
+                <nav aria-label="Page navigation example">
+                    <ul class="pagination">
+                        <li class="page-item"><a class="page-link" onclick="paginator('prev')">Previous</a></li>
+                        @for($i=1;$i < $paginator +1; $i++)
+                        <li class="page-item"><a class="page-link" onclick="paginator('<?= $i?>')"><?= $i?></a></li>
+                        @endfor
+                        <li class="page-item"><a class="page-link" onclick="paginator('next')">Next</a></li>
+
+                    </ul>
+                </nav>
             </div>
         </div>
     </section>
@@ -92,37 +109,43 @@
                 </tr>
             </table>
         </div>
-        <form action="/admin/store" method="POST" enctype="multipart/form-data" class="">
+        <form action="/admin/store" method="POST" id="saveCityForm" enctype="multipart/form-data" class="">
         @csrf
         <input type="hidden" id="data_id" name="data_id" >
         <div class="modal-body px-4">
             <div class="form-group py-2">
                 <label class="form-label" for="data_city">{{__('cities.admin.data_city')}}</label>
-                <input id="data_city" name="data_city" class="form-control" placeholder="{{__('cities.admin.ph_city')}}"/>
+                <input id="data_city" name="data_city" required  class="form-control" placeholder="{{__('cities.admin.ph_city')}}"/>
+                <div id="validation_data_city" class="invalid-feedback">Obligatory field</div>
             </div>
             <div class="form-group py-2">
                 <label class="form-label" for="data_country">{{__('cities.admin.data_country')}}</label>
-                <input id="data_country" name="data_country" class="form-control" placeholder="{{__('cities.admin.ph_country')}}"/>
+                <input id="data_country" name="data_country" required class="form-control" placeholder="{{__('cities.admin.ph_country')}}"/>
             </div>
             <div class="form-group py-2">
                 <label class="form-label" for="data_continent">{{__('cities.admin.data_continent')}}</label>
-                <select id="data_continent" name="data_continent" class="form-control">
+                <select id="data_continent" name="data_continent" required class="form-control">
                     @foreach($continents as $continent)
                     <option value="{{ $continent['id'] }}">{{ $continent["name"] }}</option>
                     @endforeach
                 </select>
+                <div id="validation_continent" class="invalid-feedback">Obligatory field</div>
             </div>
             <div class="form-group py-2">
                 <label class="form-label" for="data_population">{{__('cities.admin.data_population')}}</label>
-                <input id="data_population" name="data_population" class="form-control" placeholder="{{__('cities.admin.ph_population')}}"/>
+                <input id="data_population" name="data_population" class="form-control"
+                            type="number" min="0" placeholder="{{__('cities.admin.ph_population')}}"/>
             </div>
             <div class="form-group py-2">
                 <label class="form-label" for="data_locations">{{__('cities.admin.data_locations')}}</label>
-                <input id="data_locations" name="data_locations" class="form-control" placeholder="{{__('cities.admin.ph_locations')}}"/>
+                <input id="data_locations" name="data_locations" class="form-control" type="number" min="0"
+                            placeholder="{{__('cities.admin.ph_locations')}}"/>
             </div>
             <div class="form-group py-2">
                 <label class="form-label" for="data_dyear">{{__('cities.admin.data_dyear')}}</label>
-                <input id="data_dyear" name="data_dyear" class="form-control" placeholder="{{__('cities.admin.ph_dyear')}}"/>
+                <input id="data_dyear" name="data_dyear" class="form-control" type="number" min="0"
+                            placeholder="{{__('cities.admin.ph_dyear')}}"/>
+                <div id="validation_data_year" class="invalid-feedback">Obligatory field</div>
             </div>
             <div class="form-group py-2 row mx-0">
                 <p class="form-label px-0" for="new_city_img">{{__('cities.admin.data_photo')}}</p>
@@ -130,15 +153,15 @@
                     <label class="custom-file-upload" for="new_city_img">
                     <input type="file" id="data_photo" onChange="sel_file()" name="data_photo"
                                         style="    cursor: pointer;    margin: 0;    opacity: 0;    outline: 0 none;    padding: 0;    position: absolute;    right: 0;    top: 0;    width: 100%;height: 80px;">
-                        <img class="mx-auto" src="{{asset('assets/icons/add_file.png')}}" width="80" height="80" id="imgFile"//>
+                        <img class="mx-auto" src="{{asset('assets/icons/add_file.png')}}" width="80" height="80" id="imgFile"/>
                     </label>
                 </div>
             </div>
         </div>
         <div class="modal-footer b-none row mx-0">
         <button type="button" class="col-4 btn btn-outline-primary ms-auto"  data-bs-dismiss="modal">{{__('admin.btn_cancel')}}</buttton>
-            <button type="button" class="col-4 btn btn-primary me-auto" id="btn_saveData"
-                         onclick="this.form.submit();">{{__('admin.btn_create')}}</buttton>
+        <button type="button" class="col-4 btn btn-primary me-auto" id="btn_saveData"
+                         onclick="saveForm()">{{__('admin.btn_create')}}</buttton>
         </div>
         </form>
     </div>
@@ -165,8 +188,66 @@
   </div>
 </div>
 
+<input type="hidden" id="search"  value="<?= $search_box?>">
 
 <script>
+
+
+    <?php if($st == '1'){?>
+        setTimeout(() => {
+            console.log("Delayed for 1 second.");
+            document.getElementById('alertMessage').style.display = 'none';
+        },5000);
+    <?php };?>
+
+
+    function saveForm(){
+        let validate = 1;
+            let data_city = document.getElementById('data_city').value;
+            //let data_country = document.getElementById('data_country').value;
+            let data_continent = document.getElementById('data_continent').value;
+            let data_dyear = document.getElementById('data_dyear').value;
+            if(data_city==''){
+                document.getElementById('data_city').className = 'form-control is-invalid';
+                document.getElementById('validation_data_city').style.display = 'block';
+                validate = 0;
+            };
+            if(data_continent==''){
+                document.getElementById('data_continent').className = 'form-control is-invalid';
+                document.getElementById('validation_continent').style.display = 'block';
+                validate = 0;
+            };
+            if(data_dyear==''){
+                document.getElementById('data_dyear').className = 'form-control is-invalid';
+                document.getElementById('validation_data_year').style.display = 'block';
+                validate = 0;
+            };
+            if(validate == 1){
+                document.getElementById('saveCityForm').submit();
+            };
+
+    }
+
+    function paginator(page){
+        //search_box
+        console.log("-->PAG");
+        document.getElementById('page').value;
+        let search = document.getElementById('search').value;
+
+        if(page == 'prev' || page == 'next'){
+
+        }else{
+            if (search == ''){
+                console.log("#not SERCH");
+                window.location = '/admin/cities/?page='+page;
+            }else{
+                console.log("# SERCH");console.log(search);
+                document.getElementById('page').value = page;
+                document.getElementById('formSerch').submit();
+            };
+        };
+    }
+
     function modalDel(id){
         document.getElementById('id_del_city').value = id;
     }
@@ -240,7 +321,7 @@
         //searchlist();
         $search_box =document.getElementById("search_box").value;
         if($search_box==''){
-        evt.preventDefault();
+            evt.preventDefault();
             window.location = '/admin/cities';
             //alert("#NO Tiene algo");
         }else{
