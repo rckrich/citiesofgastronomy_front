c10d5431aac878f4c0e916800b859f5a75a1faf3
rociomoreno1 <rmoreno@walook.com.mx>
2024-01-15 10:58:35 -0600

    CITIES - listado landing - agregado de cantItems

--- citiesofgastronomy/app/Http/Controllers/LandingController.php
+++ citiesofgastronomy/app/Http/Controllers/LandingController.php
@@ -45,8 +45,10 @@ class LandingController extends Controller
     }
     public function cities()
     {
+        $cantItems = 99999;
+
         Log::info(config('app.apiUrl'));
-        $url = config('app.apiUrl').'cities';
+        $url = config('app.apiUrl').'cities/?cantItems='.$cantItems;
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
