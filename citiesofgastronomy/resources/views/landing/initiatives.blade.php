20305f6dcb10294faafea1fcf546a41461799ec1
HCruzWalook <hcruz@walook.com.mx>
2024-01-24 13:11:44 -0600

    front fix - contact initiatives

--- citiesofgastronomy/resources/views/landing/initiatives.blade.php
+++ citiesofgastronomy/resources/views/landing/initiatives.blade.php
@@ -35,42 +35,42 @@
 
         <div class="container px-lg-5 px-md-5 px-sm-3 px-3 pt-5 filters">
         <div class="row g-6">
-            <div class="col-lg-2 col-md-4 col-sm-6 col-6 my-3">
+            <div class="col-lg-2 col-md-4 col-sm-6 col-6 pe-lg-1 ps-lg-2 px-md-1 px-sm-1 px-1 my-3">
                 <div class="form-group">
                     <select class="form-control">
                         <option id="filter-" name="filter-" value="0">Cities</option>
                     </select>
                 </div>
             </div>
-            <div class="col-lg-2 col-md-4 col-sm-6 col-6 my-3">
+            <div class="col-lg-2 col-md-4 col-sm-6 col-6 px-1 my-3">
                 <div class="form-group">
                     <select class="form-control">
                         <option id="filter-" name="filter-" value="0">Type of Activity</option>
                     </select>
                 </div>
             </div>
-            <div class="col-lg-2 col-md-4 col-sm-6 col-6 my-3">
+            <div class="col-lg-2 col-md-4 col-sm-6 col-6 px-1 my-3">
                 <div class="form-group">
                     <select class="form-control">
                         <option id="filter-" name="filter-" value="0">Topics</option>
                     </select>
                 </div>
             </div>
-            <div class="col-lg-2 col-md-4 col-sm-6 col-6 my-3">
+            <div class="col-lg-2 col-md-4 col-sm-6 col-6 px-1 my-3">
                 <div class="form-group">
                     <select class="form-control">
                         <option id="filter-" name="filter-" value="0">SDG</option>
                     </select>
                 </div>
             </div>
-            <div class="col-lg-2 col-md-4 col-sm-6 col-6 my-3">
+            <div class="col-lg-2 col-md-4 col-sm-6 col-6 px-1 my-3">
                 <div class="form-group">
                     <select class="form-control">
                         <option id="filter-" name="filter-" value="0">Connection to other creative fields</option>
                     </select>
                 </div>
             </div>
-            <div class="col-lg-2 col-md-4 col-sm-6 col-6 my-3">
+            <div class="col-lg-2 col-md-4 col-sm-6 col-6 ps-lg-1 pe-lg-2 px-md-1 px-sm-1 px-1 my-3">
                 <button class="btn btn-info w-100 h-100">Filter posts</button>
             </div>
         </div>
@@ -80,7 +80,9 @@
         <div class="row g-4">
                 <div class="col-lg-3 col-md-4 col-sm-6 col-12">
                     <div class="card">
-                        <img src="{{asset('assets/img/Home/sample.png')}}" class="card-img-top" alt="...">
+                        <a href="{{route('initiatives.view')}}" class="img-link">
+                            <img src="{{asset('assets/img/Home/sample.png')}}" class="card-img-top" alt="...">
+                        </a>
                         <div class="card-body px-0 bg-black text-white">
                             <h5 class="card-title mb-2">{{__('landing.lorem_title')}}</h5>
                             <p class="card-text mb-2">{{__('landing.lorem')}}</p>
@@ -93,7 +95,7 @@
                             </div>
                             <h6 class="text-blue activity mb-2"><b>{{__('landing.activity_type')}}</b></h6>
                             <p class="card-text mb-2">{{__('landing.initiatives.lorem_activity')}}</p>
-                            <a href="{{route('landing.initiatives')}}" class="btn btn-link px-0">{{__('landing.btn_read')}}</a>
+                            <a href="{{route('initiatives.view')}}" class="btn btn-link px-0">{{__('landing.btn_read')}}</a>
                         </div>
                     </div>
                 </div>
@@ -112,7 +114,7 @@
                             </div>
                             <h6 class="text-blue activity mb-2"><b>{{__('landing.activity_type')}}</b></h6>
                             <p class="card-text mb-2">{{__('landing.initiatives.lorem_activity')}}</p>
-                            <a href="{{route('landing.initiatives')}}" class="btn btn-link px-0">{{__('landing.btn_read')}}</a>
+                            <a href="{{route('initiatives.view')}}" class="btn btn-link px-0">{{__('landing.btn_read')}}</a>
                         </div>
                     </div>
                 </div>
@@ -131,7 +133,7 @@
                             </div>
                             <h6 class="text-blue activity mb-2"><b>{{__('landing.activity_type')}}</b></h6>
                             <p class="card-text mb-2">{{__('landing.initiatives.lorem_activity')}}</p>
-                            <a href="{{route('landing.initiatives')}}" class="btn btn-link px-0">{{__('landing.btn_read')}}</a>
+                            <a href="{{route('initiatives.view')}}" class="btn btn-link px-0">{{__('landing.btn_read')}}</a>
                         </div>
                     </div>
                 </div>
@@ -150,7 +152,7 @@
                             </div>
                             <h6 class="text-blue activity mb-2"><b>{{__('landing.activity_type')}}</b></h6>
                             <p class="card-text mb-2">{{__('landing.initiatives.lorem_activity')}}</p>
-                            <a href="{{route('landing.initiatives')}}" class="btn btn-link px-0">{{__('landing.btn_read')}}</a>
+                            <a href="{{route('initiatives.view')}}" class="btn btn-link px-0">{{__('landing.btn_read')}}</a>
                         </div>
                     </div>
                 </div>
@@ -169,7 +171,7 @@
                             </div>
                             <h6 class="text-blue activity mb-2"><b>{{__('landing.activity_type')}}</b></h6>
                             <p class="card-text mb-2">{{__('landing.initiatives.lorem_activity')}}</p>
-                            <a href="{{route('landing.initiatives')}}" class="btn btn-link px-0">{{__('landing.btn_read')}}</a>
+                            <a href="{{route('initiatives.view')}}" class="btn btn-link px-0">{{__('landing.btn_read')}}</a>
                         </div>
                     </div>
                 </div>
